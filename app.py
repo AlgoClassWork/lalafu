3b32a3656441cd91d93b52e5850b7b89b8bcb0bd
AlgoClassWork <kyxec.play@gmail.com>
2025-01-25 13:42:34 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import database
 
 app = Flask(__name__)
@@ -8,5 +8,12 @@ database.create_db()
 def index():
     ads = database.read_ad()
     return render_template('index.html', ads=ads)
+
+@app.route('/create', methods=['POST'])
+def create():
+    title = request.form.get('title')
+    content = request.form.get('content')
+    database.create_ad(title, content)
+    return redirect(url_for('index'))
 
 app.run()
