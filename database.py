ba0148bf76f28f2434d1be98f8d31bcb30e23c69
AlgoClassWork <kyxec.play@gmail.com>
2025-01-25 13:07:44 +0600

    Update database.py

--- database.py
+++ database.py
@@ -7,6 +7,35 @@ def create_db():
     CREATE TABLE IF NOT EXISTS ad (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
-    content TEXT NOT NULL)  ''')
+    content TEXT NOT NULL) ''')
+    connection.commit()
+    connection.close()
+
+def create_ad(title, content):
+    connection = sqlite3.connect('ad.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO ad (title, content) VALUES (?,?)', (title, content))
+    connection.commit()
+    connection.close()
+
+def read_ad():
+    connection = sqlite3.connect('ad.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM ad')
+    ads = cursor.fetchall()
+    connection.close()
+    return ads
+
+def update_ad(id, title, content):
+    connection = sqlite3.connect('ad.db')
+    cursor = connection.cursor()
+    cursor.execute('UPDATE ad SET title=?, content=? WHERE id=?', (title, content, id))
+    connection.commit()
+    connection.close()
+
+def delete_ad(id):
+    connection = sqlite3.connect('ad.db')
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM ad WHERE id=?', (id))
     connection.commit()
     connection.close()
