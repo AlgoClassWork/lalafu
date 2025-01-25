06ca895f57b263325735f32eb6fc2c047575525f
AlgoClassWork <kyxec.play@gmail.com>
2025-01-25 14:07:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
-import database
+import database as db
 
 app = Flask(__name__)
-database.create_db()
+db.init_db()
 
-@app.route('/')
+@app.route("/")
 def index():
-    ads = database.read_ad()
-    return render_template('index.html', ads=ads)
+    ads = db.get_ads()
+    return render_template("index.html", ads=ads)
 
-@app.route('/create', methods=['POST'])
+@app.route("/create", methods=["POST"])
 def create():
-    title = request.form.get('title')
-    content = request.form.get('content')
-    database.create_ad(title, content)
-    return redirect(url_for('index'))
+    title = request.form.get("title")
+    content = request.form.get("content")
+    if title and content:
+        db.create_ad(title, content)
+    return redirect(url_for("index"))
 
-@app.route('/update/<int:id>', methods=['POST'])
-def update(id):
-    title = request.form.get('title')
-    content = request.form.get('content')
-    database.update_ad(id, title, content)
-    return redirect(url_for('index'))
+@app.route("/update/<int:ad_id>", methods=["POST"])
+def update(ad_id):
+    title = request.form.get("title")
+    content = request.form.get("content")
+    if title and content:
+        db.update_ad(ad_id, title, content)
+    return redirect(url_for("index"))
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    database.delete_ad(id)
-    return redirect(url_for('index'))
+@app.route("/delete/<int:ad_id>")
+def delete(ad_id):
+    db.delete_ad(ad_id)
+    return redirect(url_for("index"))
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
